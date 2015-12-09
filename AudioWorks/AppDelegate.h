5feeee39ef3faf34d0cf2dfbf6699a082859f4de
Jeff Gregorio <Jeff@n2-101-96.dhcp.drexel.edu>
2015-12-09 13:48:17 -0500

    Initial Commit

---
+++ AudioWorks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioWorks
+//
+//  Created by Jeff Gregorio on 12/9/15.
+//  Copyright © 2015 Jeff Gregorio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AudioWorks/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AudioWorks
+//
+//  Created by Jeff Gregorio on 12/9/15.
+//  Copyright © 2015 Jeff Gregorio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
